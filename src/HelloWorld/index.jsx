1f63cd61ffc2ff76cc45f2f0f4c257a885269fe3
Julio Derill Juan Weol <weoljulio03@gmail.com>
2025-11-06 09:06:30 +0800

    event handler & state

--- src/HelloWorld/index.jsx
+++ src/HelloWorld/index.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import { useState } from "react";
 import HelloMoon from "../HelloMoon";
+//Tipe data props adalah Object
 
-//tipe data props adalah object
-
-const HelloWorld = ({text}) => {
+const HelloWorld = ({ text, handleClick }) => {
+  const [welcome, setWelcome] = useState("Welcome");
+  const handleClickChild = () => {
+    setWelcome("Selamat Datang");
+  };
   return (
     <>
-      <div className="text-[50px] bg-red-500 m-10 p-10 font-serif">
+      <div className="text-[25px] bg-red-500 m-10 p-10 font-serif">
         {text}
+        {welcome}
         <HelloMoon />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleClick}
+        >
+          Click Me
+        </button>
+        <button
+          className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleClickChild}
+        >
+          Click Me
+        </button>
       </div>
     </>
   );
